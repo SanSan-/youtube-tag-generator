93823a95385243e57d085d8cf890cb4de0ea7e1b
Alex Franz <ut1339@gmail.com>
2021-11-29 02:46:18 +0300

    [tech debt] load stress testing fix

--- rest-server/server.js
+++ rest-server/server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '200mb' }));
+app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
 
 const port = 3003;
 require('./app/routes')(app);
